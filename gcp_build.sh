00a215fa8f31f1d499f38cc9203aedf74ef37d0e
jainahik <36563435+jainahik@users.noreply.github.com>
2019-11-19 00:07:11 +0530

    Update gcp_build.sh

--- gcp_build.sh
+++ gcp_build.sh
@@ -2,16 +2,16 @@
 
 pwd
 
-echo "jenkins build number is $build_number"
+#echo "jenkins build number is $build_number"
 
-gsutil cp gs://spinnaker-poc-artifact-bucket/spinnaker-gcp-"$build_number".tar .
+#gsutil cp gs://spinnaker-poc-artifact-bucket/spinnaker-gcp-"$build_number".tar .
 
-sudo mv spinnaker-gcp-"$build_number".tar /var/www/html
+sudo tar -xzf spinnaker-gcp-"$build_number".tar
+
+sudo mv index.html /var/www/html
 
 cd /var/www/html
 
 #sudo sh -c "echo 'This is gcp spinnaker check' > index.html"
-
-sudo tar -xzf spinnaker-gcp-"$build_number".tar
 
 sudo rm -rf spinnaker-gcp-"$build_number".tar
