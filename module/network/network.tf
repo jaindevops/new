580ddf389d3ac1919ce277be50dccca6f0bd8a15
Abhishek <jain.abhi198@gmail.com>
2023-10-28 22:23:08 +0530

    module modified

--- module/network/network.tf
+++ module/network/network.tf
@@ -1,8 +1,12 @@
+## Variable 
+variable "project_id" { type = string}
+
 /***************************
        Google Network
     ***************************/
-resource "google_compute_network" "vpc_network_01" {
-  name                    = "vpc-01"
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc"
   auto_create_subnetworks = false
   routing_mode            = "GLOBAL"
+  project                 = "var.project_id"
 }
--- module/network/output.tf
+++ module/network/output.tf
@@ -1,10 +1,14 @@
 // VPC outputs
 output "vpc_name" {
-  value = google_compute_network.vpc_network_01.name
+  value = google_compute_network.vpc_network.name
 }
 output "vpc_id" {
-  value = google_compute_network.vpc_network_01.id
+  value = google_compute_network.vpc_network.id
 }
 output "vpc_self_link" {
-  value = google_compute_network.vpc_network_01.self_link
+  value = google_compute_network.vpc_network.self_link
+}
+
+output "project_name" {
+  value = google_compute_network.vpc_network.project
 }
